f95c12fdb7bdae83509f9771797a779748d16fdc
Denis Scholokov <scholokov@users.noreply.github.com>
2025-04-27 13:58:51 +0300

    feat: bootstrap sanity studio

---
+++ sanity.config.js
@@ -0,0 +1,21 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: '4u5lbqc5',
+
+  projectId: '874v0tp0',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
